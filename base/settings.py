50a84862aca818afc13d5b147bf83216c861ac04
gaut <gaut@b3pc.lan>
2017-09-26 15:49:20 +0200

    add domain name to allowed hosts

--- base/settings.py
+++ base/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['denisverm.pythonanywhere.com', 'gautierb3.pythonanywhere.com', 'holderbase.com']
+ALLOWED_HOSTS = ['denisverm.pythonanywhere.com', 'gautierb3.pythonanywhere.com', 'www.holderbase.com']
 
 
 # Application definition
